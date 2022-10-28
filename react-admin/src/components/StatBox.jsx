3c7dbf30b22f0fc3ca36a794be837cfc953be8ef
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-10-28 19:51:00 +0700

    components: create ProgressCircle

---
+++ react-admin/src/components/StatBox.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography,useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircle from "./ProgressCircle";
+
+const StatBox = ({title, subtitle, icon, progress, increase}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        
+    )
+}
