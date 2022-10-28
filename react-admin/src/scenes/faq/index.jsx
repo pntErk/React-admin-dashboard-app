c4787f3b9d36abd5e7844903e2adca4f0fa7e4a6
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-10-28 13:41:04 +0700

    faq: add accrodion&summary

--- react-admin/src/scenes/faq/index.jsx
+++ react-admin/src/scenes/faq/index.jsx
@@ -1,13 +1,103 @@
 import { Box, useTheme, Typography } from "@mui/material";
 import Header from "../../components/Header";
-import Accordion from "@mui/material";
+import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { tokens } from "../../theme";
 
 const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-}
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            est animi vel velit doloremque necessitatibus enim in cupiditate
+            eligendi error officia, commodi ipsa laboriosam eum maxime aperiam
+            dolore. Neque, quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            est animi vel velit doloremque necessitatibus enim in cupiditate
+            eligendi error officia, commodi ipsa laboriosam eum maxime aperiam
+            dolore. Neque, quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Favourite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            est animi vel velit doloremque necessitatibus enim in cupiditate
+            eligendi error officia, commodi ipsa laboriosam eum maxime aperiam
+            dolore. Neque, quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            est animi vel velit doloremque necessitatibus enim in cupiditate
+            eligendi error officia, commodi ipsa laboriosam eum maxime aperiam
+            dolore. Neque, quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            The Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            est animi vel velit doloremque necessitatibus enim in cupiditate
+            eligendi error officia, commodi ipsa laboriosam eum maxime aperiam
+            dolore. Neque, quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      
+    </Box>
+  );
+};
 
 export default FAQ;
