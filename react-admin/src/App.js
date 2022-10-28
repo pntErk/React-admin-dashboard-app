7bdefbee92893971cfde7b60a8cad66fd67a82f4
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-10-28 13:50:34 +0700

    components: create BarChart
    using nivo charts

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -10,11 +10,10 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
-
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -34,8 +33,8 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-              <Route path="/faq" element={<FAQ/>} />
-              {/* <Route path="/bar" element={<Bar/>} /> */}
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
               {/* <Route path="/line" element={<Line/>} /> */}
               {/* <Route path="/pie" element={<Pie/>} /> */}
               {/* <Route path="/geography" element={<Geography/>} /> */}
---
+++ react-admin/src/scenes/bar/index.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
+
+const Bar = () => {
+
+}
+
+export default Bar
