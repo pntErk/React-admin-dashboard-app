7434cdd938c2cb41d0e950305939580e58c2e41b
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-10-28 14:08:30 +0700

    components: create linechart

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -12,8 +12,7 @@ import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
 import Pie from "./scenes/pie";
-// import Line from "./scenes/line";
-
+import Line from "./scenes/line";
 // import Geography from "./scenes/geography";
 
 function App() {
@@ -37,8 +36,7 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/line" element={<Line/>} /> */}
-
+              <Route path="/line" element={<Line/>} />
               {/* <Route path="/geography" element={<Geography/>} /> */}
             </Routes>
           </main>
